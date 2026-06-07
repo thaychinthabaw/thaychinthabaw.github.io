d8bb7e302ed906479c3c1e6a4a26af794d010d67
thaychinthabaw <thaychinthabaw@gmail.com>
2026-06-07 21:29:41 +0630

    Create text1.js

---
+++ text1.js
@@ -0,0 +1,5 @@
+// text1.js ဖိုင်အတွင်းတွင် ရေးရမည့်ပုံစံ
+export const novelChapters1 = [
+    { id: "chapter-4", content: "အခန်း ၄ ပါဝင်မည့် စာသားများ..." },
+    { id: "chapter-5", content: "အခန်း ၅ ပါဝင်မည့် စာသားများ..." }
+];
